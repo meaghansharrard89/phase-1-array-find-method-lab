2414d2f30a3a878c1b865a52018d994e5e542240
Meaghan <meaghan.sharrard89@gmail.com>
2023-11-03 16:06:16 -0700

    passing tests

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ]
+
+  function superbowlWin(array) {
+    const foundWin = array.find((element) => {
+        if (element.result === 'W') {
+        return element;
+        }
+    }) 
+    if (foundWin === undefined) {
+        return undefined;
+    }
+    return foundWin.year;
+}
+
+superbowlWin(record);
